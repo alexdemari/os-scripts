0532c7bb1ef6dddfcd72a5b45b742c89a92d7b41
Alexandre de Mari <alexandre@somosleve.com.br>
2022-12-19 14:52:03 -0300

    Set nano as defautl git editor

--- ubuntu/python-setup.sh
+++ ubuntu/python-setup.sh
@@ -23,6 +23,7 @@ git_config() {
   git config --global alias.merc "merge --no-ff"
   git config --global alias.grog "log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)'"
   git config --global push.default simple
+  git config --global core.editor "nano"
 }
 
 install_pyenv() {
